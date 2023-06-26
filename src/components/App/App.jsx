d08ee1fd9289df34d3298ba0fe31faf0d8ca15e0
VeronikaVS90 <vsokolovanika@gmail.com>
2023-06-27 00:37:34 +0300

    added storage of contacts in localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,14 +13,32 @@ import { Filter } from 'components/Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    try {
+      const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+
+      if (savedContacts && savedContacts.length > 0) {
+        this.setState({
+          contacts: savedContacts,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const previousContacts = prevState.contacts;
+    const updatedContacts = this.state.contacts;
+
+    if (updatedContacts.length !== previousContacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+    }
+  }
 
   handleFormSubmit = ({ name, number }) => {
     const normalizedName = name.toLowerCase();
